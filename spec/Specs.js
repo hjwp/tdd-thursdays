931483f6612a55e8695cca3fbe1d6074094cc80c
James Gough <james.gough@made.com>
2019-01-31 16:45:07 +0000

    another commit

--- spec/Specs.js
+++ spec/Specs.js
@@ -5,7 +5,7 @@ describe("A feature", () => {
   let rover;
 
   beforeEach(function() {
-    rover = ourModule.rover;
+    rover = new ourModule.rover();
   });
 
   it("should start at zero and face north", function() {
@@ -14,5 +14,15 @@ describe("A feature", () => {
     expect(rover.direction).toEqual('N');
   });
 
+  it("should be able to receive a char array to move", function() {
+    rover.x = 0;
+    rover.y = 0;
+    rover.direction = 'N';
+    let char_array = ['f'];
+    rover.move(char_array);
+    expect(rover.x).toEqual(0);
+    expect(rover.y).toEqual(1);
+    expect(rover.direction).toEqual('N');
+  });
 
 });
